3eccefe20b417e2f874d14fe0b59e2128dbec1b6
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-10 18:22:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,12 +9,11 @@ class School
   end
   
   def roster
-    Hash.new 
+    school_roster = Hash.new 
   end
   
   def add_student(name, grade)
-    self.roster[:grade] = []
-    self.roster[:grade] << name
+    
   end
   
 end
