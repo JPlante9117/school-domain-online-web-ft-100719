d172a55b7020dad55f211645472f4359e27c3c92
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-10 18:17:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,15 @@
 class School
   
-  attr_accessor :roster
+  attr_accessor 
   attr_reader :name
   
   def initialize(name)
     @name = name
+    roster
+  end
+  
+  def roster
+    Hash.new 
   end
   
 end
